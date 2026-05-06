7e1068769d35534d7bcc7203601ff650d2e9b741
sharu <sharu@DESKTOP-4CD36IM>
2026-05-06 20:51:55 +0300

    Lombok constructors were added

--- app/src/main/java/com/example/fitnesscalendar/relations/ExerciseCategoryCrossRef.java
+++ app/src/main/java/com/example/fitnesscalendar/relations/ExerciseCategoryCrossRef.java
@@ -4,9 +4,14 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Index;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 // bridge - join table
 
 // unique row in this table is the combination of an exercise and category
+@NoArgsConstructor // required by Room
+@AllArgsConstructor
 @Entity(
         tableName = "exercise_category_cross_ref",
         primaryKeys = {"exercise_id", "category_id"},
