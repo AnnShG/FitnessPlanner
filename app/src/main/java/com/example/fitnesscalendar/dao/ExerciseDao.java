bef7fabd43633e2d9e037c37c989f48a7c96bdbd
sharu <sharu@DESKTOP-4CD36IM>
2026-05-07 18:43:31 +0300

    Reordered the file

--- app/src/main/java/com/example/fitnesscalendar/dao/ExerciseDao.java
+++ app/src/main/java/com/example/fitnesscalendar/dao/ExerciseDao.java
@@ -26,7 +26,7 @@ public interface ExerciseDao {
 
     // method creates the "Link" between an Exercise and a Category.
     @Insert(onConflict = OnConflictStrategy.REPLACE) // prevents linking the same Ex to the same Cat
-    void insertCategoryCrossRef(ExerciseCategoryCrossRef crossRef);
+    void insertExerciseCategoryCrossRef(ExerciseCategoryCrossRef crossRef);
 
     @Transaction // required because Room runs several queries (exercises, steps, categories)
     @Query("SELECT * FROM exercises")
@@ -35,28 +35,15 @@ public interface ExerciseDao {
     @Query("SELECT * FROM exercises")
     LiveData<List<ExerciseSummary>> getExerciseSummaries();
 
-    // Get ALL exercises
-    @Transaction
-    @Query("SELECT * FROM exercises WHERE owner_id IS NULL or owner_id = :userId")
-    LiveData<List<FullExerciseRecord>> getAvailableExercises(long userId);
+    @Query("SELECT * FROM exercises")
+    LiveData<List<Exercise>> getAllExercisesOnly();
 
-    // Get ONLY the exercises created by the user:
-//    @Transaction
-//    @Query("SELECT * FROM exercises WHERE owner_id = :userId")
-//    LiveData<List<FullExerciseRecord>> getMyCustomExercises(long userId);
-
-//    @Transaction
-//    @Query("SELECT * FROM exercises WHERE owner_id IS NULL")
-//    LiveData<List<FullExerciseRecord>> getPreDefinedExercises();
-
-    @Transaction
-    @Query("SELECT * FROM exercises WHERE exercise_id = :exerciseId")
-    LiveData<FullExerciseRecord> getExerciseById(long exerciseId);
+    @Query("SELECT * FROM exercise_category_cross_ref WHERE exercise_id = :exId")
+    List<ExerciseCategoryCrossRef> getCrossRefsForExercise(long exId);
 
     @Transaction
     @Query("SELECT * FROM exercises WHERE exercise_id = :id")
     LiveData<FullExerciseRecord> getFullExerciseById(long id);
-
 
     @Delete
     void delete(Exercise exercise);
@@ -65,10 +52,11 @@ public interface ExerciseDao {
     @Query("DELETE FROM steps WHERE exercise_id = :exerciseId")
     void deleteStepsByExerciseId(long exerciseId);
 
-    // Delete existing category links
+    // Delete existing categories before inserting new ones to avoid duplicates
     @Query("DELETE FROM exercise_category_cross_ref WHERE exercise_id = :exerciseId")
     void deleteCategoryCrossRefsByExerciseId(long exerciseId);
 
+    // TEST
     @Transaction
     @Query("SELECT DISTINCT e.* FROM exercises e " +
             "INNER JOIN exercise_category_cross_ref ec ON e.exercise_id = ec.exercise_id " +
@@ -79,4 +67,18 @@ public interface ExerciseDao {
     @Transaction
     @Query("SELECT * FROM exercises WHERE :searchQuery IS NULL OR title LIKE '%' || :searchQuery || '%'")
     LiveData<List<FullExerciseRecord>> getExercisesBySearchOnly(String searchQuery);
+
+    // Get ALL exercises
+//    @Transaction
+//    @Query("SELECT * FROM exercises WHERE owner_id IS NULL or owner_id = :userId")
+//    LiveData<List<FullExerciseRecord>> getAvailableExercises(long userId);
+
+    // Get ONLY the exercises created by the user:
+//    @Transaction
+//    @Query("SELECT * FROM exercises WHERE owner_id = :userId")
+//    LiveData<List<FullExerciseRecord>> getMyCustomExercises(long userId);
+
+//    @Transaction
+//    @Query("SELECT * FROM exercises WHERE owner_id IS NULL")
+//    LiveData<List<FullExerciseRecord>> getPreDefinedExercises();
 }
