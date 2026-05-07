137505cd1ab6f10c5e95774a29db011f50f978fc
sharu <sharu@DESKTOP-4CD36IM>
2026-05-07 18:37:35 +0300

    Impplemented cascade on delete for ExerciseCategoryCrossRef

--- app/src/main/java/com/example/fitnesscalendar/relations/ExerciseCategoryCrossRef.java
+++ app/src/main/java/com/example/fitnesscalendar/relations/ExerciseCategoryCrossRef.java
@@ -2,7 +2,11 @@ package com.example.fitnesscalendar.relations;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Index;
+
+import com.example.fitnesscalendar.entities.Category;
+import com.example.fitnesscalendar.entities.Exercise;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -15,7 +19,21 @@ import lombok.NoArgsConstructor;
 @Entity(
         tableName = "exercise_category_cross_ref",
         primaryKeys = {"exercise_id", "category_id"},
-        indices = {@Index("category_id")}
+        indices = {@Index("category_id")},
+        foreignKeys = {
+                @ForeignKey(
+                        entity = Exercise.class,
+                        parentColumns = "exercise_id",
+                        childColumns = "exercise_id",
+                        onDelete = ForeignKey.CASCADE
+                ),
+                @ForeignKey(
+                        entity = Category.class,
+                        parentColumns = "category_id",
+                        childColumns = "category_id",
+                        onDelete = ForeignKey.CASCADE
+                )
+        }
 )
 public class ExerciseCategoryCrossRef {
     @ColumnInfo(name = "exercise_id")
