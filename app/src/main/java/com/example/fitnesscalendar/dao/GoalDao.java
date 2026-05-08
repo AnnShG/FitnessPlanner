8b6b45a2d49a16f2142e5e258b9e28d141ecbd8d
sharu <sharu@DESKTOP-4CD36IM>
2026-05-08 17:58:00 +0300

    New method

--- app/src/main/java/com/example/fitnesscalendar/dao/GoalDao.java
+++ app/src/main/java/com/example/fitnesscalendar/dao/GoalDao.java
@@ -13,17 +13,18 @@ import java.util.List;
 @Dao
 public interface GoalDao {
     @Insert
-    void insert(Goal goal);
+    long insert(Goal goal);
 
     @Update
     void update(Goal goal);
 
-    @Delete
-    void delete(Goal goal);
-
+    //
     // get all goals for a specific user
     @Query("SELECT * FROM goals WHERE user_id = :userId")
     List<Goal> getGoalsForUser(long userId);
+
+    @Query("SELECT * FROM goals WHERE goal_id = :goalId")
+    Goal getGoalById(long goalId);
 
     /**
      * Deletes all goals associated with a specific user.
