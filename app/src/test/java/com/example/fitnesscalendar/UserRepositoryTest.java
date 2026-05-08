4c2461b637aa5cea52703916cf78f0753af1fd7c
sharu <sharu@DESKTOP-4CD36IM>
2026-05-08 18:03:52 +0300

    Removed imports

--- app/src/test/java/com/example/fitnesscalendar/UserRepositoryTest.java
+++ app/src/test/java/com/example/fitnesscalendar/UserRepositoryTest.java
@@ -2,10 +2,7 @@ package com.example.fitnesscalendar;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
